11f62f922249e92a8226e0298b4b5fd751a8ee13
Luni <ulkus@interia.pl>
2023-02-19 13:44:42 +0100

    created country list

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into country (id, name, code) values (1, 'Poland', 'pl');
+insert into country (id, name, code) values (2, 'Germany', 'de');
+insert into country (id, name, code) values (3, 'USA', 'us');
+insert into country (id, name, code) values (4, 'Spain', 'es');
